222f3fe2183288063ba612adc272f8e976902eb1
Nicolas Rincon <nicorincon02@gmail.com>
2025-05-20 22:41:49 -0500

    backup 20/05

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -80,13 +80,13 @@ const Navbar = () => {
             <div className="flex items-center">
               <div className="flex-shrink-0">
                 <div className="flex items-center">
-                  <span className="text-xs text-gray-500 mr-2">Cali/Colombia</span>
+                  <span className="text-xs text-gray-500 mr-2">Asistente virtual</span>
                   <span className="text-2xl font-bold">
                     <span className="text-3xl">L</span>
                     <span>IA</span>
                   </span>
                 </div>
-                <div className="text-xs text-gray-600 ml-6">Asistente de Moda Virtual</div>
+                <div className="text-xs text-gray-600 ml-6">By Consuelo Guzmán</div>
               </div>
             </div>
 
